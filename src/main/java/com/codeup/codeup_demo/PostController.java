d8170fb49fd7966e1b4ba2060d7209e8cd408f5b
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-03-23 16:08:23 -0400

    Post Controller

---
+++ src/main/java/com/codeup/codeup_demo/PostController.java
@@ -0,0 +1,35 @@
+package com.codeup.codeup_demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+
+	@GetMapping("/posts")
+	@ResponseBody
+	public String getPosts() {
+		return "View all posts";
+	}
+
+	@GetMapping("/posts/{id}")
+	@ResponseBody
+	public String getPost(@PathVariable String id) {
+		return "View individual post. ID: " + id;
+	}
+
+	@GetMapping("/posts/create")
+	@ResponseBody
+	public String viewCreatePost() {
+		return "Post creation form.";
+	}
+
+	@PostMapping("/posts/create")
+	@ResponseBody
+	public String createPost() {
+		return "Post created.";
+	}
+}
