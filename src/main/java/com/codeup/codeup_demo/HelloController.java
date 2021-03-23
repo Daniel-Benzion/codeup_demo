6750516eb1376f5249599570861755dba1199b79
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-03-23 15:02:40 -0400

    Hello Controller

--- src/main/java/com/codeup/codeup_demo/HelloController.java
+++ src/main/java/com/codeup/codeup_demo/HelloController.java
@@ -1,8 +1,7 @@
 package com.codeup.codeup_demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 class HelloController {
@@ -11,5 +10,11 @@ class HelloController {
 	@ResponseBody
 	public String hello() {
 		return "Hello from Spring!";
+	}
+
+	@RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+	@ResponseBody
+	public String addOne(@PathVariable int number) {
+		return number + " plus one is " + (number + 1) + "!";
 	}
 }
